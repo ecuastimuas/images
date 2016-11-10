9824f02b11bdafb990e06ae3ccfbdae8f7ca50e9
Leo Leimser <leo.leimser@fusonic.net>
2016-11-10 11:16:57 +0100

    Optimizing gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,51 @@
 const gulp              = require('gulp');
+const path              = require('path');
 const autoprefixer      = require('gulp-autoprefixer');
+const clean             = require('gulp-clean');
 const concat            = require('gulp-concat');
 const cssnano           = require('gulp-cssnano');
-const rename            = require('gulp-rename');
 const less              = require('gulp-less');
-const path              = require('path');
+const rename            = require('gulp-rename');
+const size              = require('gulp-size');
+const runSequence       = require('run-sequence');
+const uglify            = require('gulp-uglifyjs');
+const watch             = require('gulp-watch');
 
 
-gulp.task('less', function() {
+gulp.task('default', function () {
+    runSequence('clean', ['less', 'js', 'static']);
+});
 
+gulp.task('clean', function () {
+    return gulp.src([
+            'assets/css',
+            'assets/js',
+            'assets/img'
+        ])
+        .pipe(clean({force: true}));
+});
+
+
+
+gulp.task('watch', function () {
+    gulp.watch('src/less/**/*.less', ['less']);
+    gulp.watch('src/js/**/*.js', ['js']);
+});
+
+
+
+gulp.task('js', function () {
+    return gulp.src([
+            'bower_components/jquery/dist/jquery.min.js',
+            'bower_components/isotope/dist/istotope.pkgd.min.js',
+            'src/js/**/*.js'
+        ])
+        .pipe(uglify('script.min.js'))
+        .pipe(size())
+        .pipe(gulp.dest('assets/js'));
+});
+
+gulp.task('less', function () {
     return gulp.src('src/less/style.less')
         .pipe(less({
             paths: [ path.join(__dirname, 'less', 'includes') ]
@@ -17,5 +54,12 @@ gulp.task('less', function() {
         .pipe(autoprefixer('last 2 version'))
         .pipe(rename({suffix: '.min'}))
         .pipe(cssnano())
+        .pipe(size())
         .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('static', function () {
+   return gulp.src('src/img/**/*')
+       .pipe(size())
+       .pipe(gulp.dest('assets/img'));
 });
