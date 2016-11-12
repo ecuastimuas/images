cbbc0bcf777689eeac725394804fe353289e7a0b
Leo Leimser <leo.leimser@fusonic.net>
2016-11-12 16:38:07 +0100

    Switched to masonry layout

--- _data/snippets/processImage.php
+++ _data/snippets/processImage.php
@@ -8,15 +8,32 @@ properties: 'a:0:{}'
 $filename = $_FILES['file']['tmp_name'];
 $destinationFile = 'X:/xampp/htdocs/images/assets/uploads/' . $_FILES['file']['name'];
 
+
 if (move_uploaded_file($filename, $destinationFile)) {
+    $imgSize = getimagesize($destinationFile);
+    $imgProp = $imgSize[0] / $imgSize[1];
+    
+    $options = 'w=600&h=600&zc=1&q=80';
+    $class = '';
+    
+    if ($imgProp < (4/3) && $imgProp > (3/4)) {
+        $options = 'w=600&h=600&zc=1&q=80';
+    } else if ($imgProp < (16/9) && $imgProp > (9/16)) {
+        $options = 'w=600&h=338&zc=1&q=80';
+        $class = 'grid-item--width2';
+    } else if ($imgProp < (9/16)) {
+        $options = 'w=338&h=600&zc=1&q=80';
+        $class = '';
+    }
     
     $thumbnail = $modx->runSnippet('pThumb', array(
         'input' => $destinationFile,
-        'options' => 'w=300'
+        'options' => $options
     ));
     
     return json_encode([
         'thumbnail' => $thumbnail,
+        'class' => $class,
         'original' => $destinationFile
     ]);
     
--- src/js/components/dropzone.js
+++ src/js/components/dropzone.js
@@ -2,21 +2,22 @@ Dropzone.autoDiscover = false;
 
 $(function() {
 
-    var $grid = $('.grid').isotope({
+    var $previewTemplate = $('#preview-template > .grid-item');
+
+    var $grid = $('.grid').masonry({
+        gutter: 24,
         itemSelector: '.grid-item',
-        layoutMode: 'masonry',
-        masonry: {
-            columnWidth: 200,
-            gutter: 20
-        }
+        columnWidth: '.grid-sizer',
+        percentPosition: true
     });
 
-    var $previewTemplate = $('#preview-template').find('.grid-item');
+    $grid.on( 'click', '.grid-item', function() {
+        $(this).toggleClass('gigante');
+        // trigger layout after item size changes
+        $grid.masonry('layout');
+    });
 
-    var $newItem = null;
-    var $itemArray = [];
-
-    $("body").dropzone({
+    $("#image-dropzone").dropzone({
         url: 'api/process-image',
         parallelUploads: 2,
         maxFilesize: 20,
@@ -32,10 +33,14 @@ $(function() {
 
             var img = JSON.parse(response.xhr.response);
 
-            var $newItem = $previewTemplate.clone();
-            $newItem.find('.dz-thumbnail').attr('src', img.thumbnail);
+            var $preview = $(response.previewElement);
+            $preview.find('.dz-thumbnail').attr('src', img.thumbnail);
+            $preview.addClass(img.class);
 
-            if (Math.random() > 0.5) {
+            $grid.masonry( 'appended', $preview );
+            $grid.masonry('layout');
+
+            /*if (Math.random() > 0.5) {
                 $newItem.addClass('grid-item--width2');
             }
 
@@ -46,7 +51,7 @@ $(function() {
             $grid.isotope()
                 .append( $newItem )
                 .isotope( 'appended', $newItem )
-                .isotope('layout');
+                .isotope('layout');*/
         }
     });
 
